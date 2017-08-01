fc0db59ea0295361bb9fad06afcfddabbb739877
teerasaksathu <maxz_drum@icloud.com>
2017-08-01 14:57:21 +0700

    setDataBaseFliebase

--- app/src/main/java/com/example/teerasaksathu/production/FromActivity.java
+++ app/src/main/java/com/example/teerasaksathu/production/FromActivity.java
@@ -3,11 +3,41 @@ package com.example.teerasaksathu.production;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
 public class FromActivity extends AppCompatActivity {
+
+    public DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_from);
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference();
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map map = (Map) dataSnapshot.getValue();
+                String vul = String.valueOf(map.get("test"));
+
+                //เหลือรอหน้าเสร็จ เพื่อเช็ค ว่างหรือ ไม่ กรอกครบ ไหม ตัวเลขเหลือป่าว
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
+
 }
