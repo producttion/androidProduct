a5180c3a18fffe251e3acaa75b36575ff09e7d31
teerasaksathu <maxz_drum@icloud.com>
2017-08-08 11:01:56 +0700

    Delete

--- app/src/main/java/com/example/teerasaksathu/production/Web_view.java
+++ app/src/main/java/com/example/teerasaksathu/production/Web_view.java
@@ -1,8 +1,12 @@
 package com.example.teerasaksathu.production;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+
 //
 public class Web_view extends AppCompatActivity {
 
@@ -12,7 +16,15 @@ public class Web_view extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
 
         String url = "https://my-project-1499687014213.firebaseapp.com";
-        WebView webView = (WebView) findViewById(R.id.web_view);
+        WebView webView = (WebView) findViewById(R.id.wed_view);
+        Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FormActivity.class);
+                startActivity(intent);
+            }
+        });
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(url);
 
