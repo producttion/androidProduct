28df275930ba566dffdd28145b19c54b74cf1034
Naetirat <naetirat.s@gmail.com>
2017-08-08 11:24:04 +0700

    Update customTabs in Web_View.java

--- app/src/main/java/com/example/teerasaksathu/production/Web_view.java
+++ app/src/main/java/com/example/teerasaksathu/production/Web_view.java
@@ -1,12 +1,10 @@
 package com.example.teerasaksathu.production;
 
-import android.content.Intent;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebView;
-import android.widget.Button;
-
 //
 public class Web_view extends AppCompatActivity {
 
@@ -16,17 +14,11 @@ public class Web_view extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
 
         String url = "https://my-project-1499687014213.firebaseapp.com";
-        WebView webView = (WebView) findViewById(R.id.wed_view);
-        Button button = (Button) findViewById(R.id.button4);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), FormActivity.class);
-                startActivity(intent);
-            }
-        });
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(url);
+//        WebView webView = (WebView) findViewById(R.id.web_view);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl(url);
+        CustomTabsIntent webview = new CustomTabsIntent.Builder().build();
+        webview.launchUrl(this, Uri.parse(url));
 
 
     }
