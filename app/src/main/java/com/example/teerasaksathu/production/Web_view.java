a5c7af0386f3c55c6ce091073aeffa0c7f98d56b
nyxlost <zeduffee@gmail.com>
2017-08-09 16:33:38 +0700

    Bar

--- app/src/main/java/com/example/teerasaksathu/production/Web_view.java
+++ app/src/main/java/com/example/teerasaksathu/production/Web_view.java
@@ -1,10 +1,10 @@
 package com.example.teerasaksathu.production;
 
-import android.net.Uri;
-import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 //
 public class Web_view extends AppCompatActivity {
 
@@ -13,12 +13,11 @@ public class Web_view extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
-        String url = "https://my-project-1499687014213.firebaseapp.com";
-//        WebView webView = (WebView) findViewById(R.id.web_view);
-//        webView.getSettings().setJavaScriptEnabled(true);
-//        webView.loadUrl(url);
-        CustomTabsIntent webview = new CustomTabsIntent.Builder().build();
-        webview.launchUrl(this, Uri.parse(url));
+        // String url = "https://my-project-1499687014213.firebaseapp.com";
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.loadUrl("https://my-project-1499687014213.firebaseapp.com");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
 
 
     }
@@ -26,6 +25,5 @@ public class Web_view extends AppCompatActivity {
 
 
 
-
-
 }
+
